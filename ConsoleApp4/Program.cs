ce29a3643fdf55baf1541f99f931ac1e86818228
leraz <leraz@DESKTOP-HIHB81H>
2023-09-26 23:22:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+/// <summary>
+/// Программа для вычисления площади трапеции
+/// </summary>
+public class Trapezoid
+{
+    /// <summary>
+    /// Константа из формулы площади трапеции
+    /// </summary>
+    int formula_const = 2;
+
+    #region private
+    /// <summary>
+    /// Верхнее основание трапеции
+    /// </summary>
+    private double topBase;
+    /// <summary>
+    /// Нижнее основание трапеции
+    /// </summary>
+    private double bottomBase;
+    /// <summary>
+    /// Высота трапеции
+    /// </summary>
+    private double height;
+    #endregion
+
+    public Trapezoid(double topBase, double bottomBase, double height)
+    {
+
+        this.topBase = topBase;
+
+        this.bottomBase = bottomBase;
+
+        this.height = height;
+    }
+
+    public double CalculateSquare()
+    {
+        if (topBase <= 0 | bottomBase <= 0 | height <= 0)
+        {
+            throw new ArgumentException("Значения должны быть положительными.");
+        }
+
+        double halfSumBases = (topBase + bottomBase) / formula_const;
+        double square = halfSumBases * height;
+
+        return square;
+    }
+}
+
+public class TrapezoidCalculation
+{
+    static void Calculate(int a, int b, int c)
+    {
+        try
+        {
+            Trapezoid trapezoid = new Trapezoid(a, b, c);
+            double square = trapezoid.CalculateSquare();
+            Console.WriteLine($"Площадь трапеции: {square}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Calculate(5, 6, 4);
+    }
+}
---
+++ ConsoleApp4Tests/TrapezoidTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class TrapezoidTests
+    {
+        /// <summary>
+        /// Хорошие тесты
+        /// </summary>
+        /// <param name="topBase"></param>
+        /// <param name="bottomBase"></param>
+        /// <param name="height"></param>
+        /// <param name="expectedSquare"></param>
+
+        [TestMethod()]
+        [DataRow(5, 8, 4, 26)]
+        [DataRow(15, 27, 14, 294)]
+
+        public void CalculateSquareTest(double topBase, double bottomBase, double height, double expectedSquare)
+        {
+            // Arrange
+            Trapezoid trapezoid = new Trapezoid(topBase, bottomBase, height);
+
+            // Act
+            double actualSquare = trapezoid.CalculateSquare();
+
+            // Assert
+            Assert.AreEqual(expectedSquare, actualSquare);
+        }
+
+        /// <summary>
+        /// Плохие тесты
+        /// </summary>
+        /// <param name="baseLength"></param>
+        /// <param name="baseWidth"></param>
+        /// <param name="height"></param>
+        /// <param name="expectedVolume"></param>
+        [TestMethod()]
+        [DataRow(20, -23, 14, 0)]
+        [ExpectedException(typeof(ArgumentException))]
+
+        public void CalculateVolumeTest_negative(double topBase, double bottomBase, double height, double expectedSquare)
+        {
+            // Arrange
+            Trapezoid trapezoid = new Trapezoid(topBase, bottomBase, height);
+
+            // Act
+            double actualSquare = trapezoid.CalculateSquare();
+
+            // Assert
+            Assert.AreEqual(expectedSquare, actualSquare);
+        }
+
+    }
+}
